6795f57c56ff5a3a3287641261d4f8f70d0f5832
Kieran Ballard <kieranballard2@gmail.com>
2019-10-03 22:08:46 +0100

    gimme a yeehaw

--- GitHubAppUpdater/Program.cs
+++ GitHubAppUpdater/Program.cs
@@ -1,17 +1,20 @@
 ﻿using System.Diagnostics;
 using System.Net;
+using System.Threading.Tasks;
 
 namespace GitHubAppUpdater
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             if (args.Length > 0)
             {
                 // Variables
                 string url = args[0];
                 string exe = args[1];
+
+                await Task.Delay(200);
              
                 var client = new WebClient();
                 client.DownloadFile(url, exe);
