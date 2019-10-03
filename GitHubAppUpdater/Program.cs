8db78c03b36e13c8782919a0f1fb59058ace7608
Kieran Ballard <kieranballard2@gmail.com>
2019-10-03 21:57:07 +0100

    fixer up

--- GitHubAppUpdater/Program.cs
+++ GitHubAppUpdater/Program.cs
@@ -1,6 +1,4 @@
-﻿using HtmlAgilityPack;
-using System;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Net;
 
 namespace GitHubAppUpdater
@@ -14,23 +12,13 @@ namespace GitHubAppUpdater
                 // Variables
                 string url = args[0];
                 string exe = args[1];
-                
-
-
-                HtmlWeb web = new HtmlWeb();
-                HtmlAgilityPack.HtmlDocument doc = web.Load(url + "latest");
-
-                Version newVersion = new Version(doc.DocumentNode.SelectNodes("//*[@id=\"js-repo-pjax-container\"]/div[2]/div[1]/div[2]/div/div[1]/ul/li[1]/a/span")[0].InnerText);
-
+             
                 var client = new WebClient();
-                client.DownloadFile(url + "download/" + newVersion + "/" + exe, exe);
+                client.DownloadFile(url, exe);
 
                 ProcessStartInfo procStartInfo = new ProcessStartInfo(exe);
                 Process.Start(procStartInfo); // Start the updated application
-
             }
-
-           
         }
     }
 }
